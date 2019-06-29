000131d6316cbbd90831d3effbda24b6844b1151
sam <pythonsamy@yahoo.com>
2019-06-29 17:49:14 +0430

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 
-const app = express()
+var app = express()
+var server = http.createServer(app)
+var io = socketIO(server)
 
+io.on('connection', (socket)=>{
+    console.log('new User conection')
+    
+    socket.on('disconected', ()=>{
+        console.log('User disconected')
+    })
+})
 app.use(express.static(publicPath))
 
 
 
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`server run on http://localhost:${port} `)
 })
